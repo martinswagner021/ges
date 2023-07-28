7e061b2cfba6235b347b7fecb6d764a19583dc43
Wagner Martins <martinswagner021@gmail.com>
2023-07-28 11:29:50 -0300

    ponto e virgula e formatacao

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-const port = 3000
+const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!');
 })
 
 app.listen(port, () => {
-    console.log(`App running on port: ${port}`)
+    console.log(`App running on port: ${port}`);
 })
