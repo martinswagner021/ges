6a0ab1a1ecdcdcd24f511669c55dddafe552c022
Wagner Martins <martinswagner021@gmail.com>
2023-07-31 10:26:49 -0300

    - JsonWebToken authentication - Routes Updates

--- src/middlewares/ensureAuth.ts
+++ src/middlewares/ensureAuth.ts
@@ -1,9 +1,12 @@
 import { Request, Response, NextFunction } from "express"
+import jwt from "jsonwebtoken"
 
 export function ensureAuth(req: Request, res: Response, next: NextFunction){
-    const authToken = req.headers.authorization
-
-    if (!authToken){
+    try {
+        const [, token] = req.headers.authorization?.split(' ')
+        const { sub } = jwt.verify(token, process.env.JWT_SECRET)
+        req.user_id = sub
+    } catch (error) {
         res.status(401).end()
     }
 
--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,22 @@ import { ensureAuth } from "./middlewares/ensureAuth.js";
 
 const router = Router()
 
-router.get('/', ensureAuth, (req, res) => res.send("Hello"))
+// Utilities
+router.post('/login') // autenticar
+
+// Create
+router.post('/relatorio', ensureAuth) // criar relatorio
+router.post('/veiculo', ensureAuth) // criar veiculo
+router.post('/cliente', ensureAuth) // criar cliente
+
+// Make changes
+router.put('/relatorio', ensureAuth) // modificar relatorio
+router.put('/veiculo', ensureAuth) // modificar veiculo
+router.put('/cliente', ensureAuth) // modificar cliente
+
+// Delete
+router.delete('/relatorio', ensureAuth) // remover relatorio
+router.delete('/veiculo', ensureAuth) // remover veiculo
+router.delete('/cliente', ensureAuth) // remover cliente
 
 export {router}
